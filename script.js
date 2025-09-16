0cb141224eb4aeaab3169c5abba433466b5c4d5c
kafilelyatim19-dev <kafil.el.yatim.19@gmail.com>
2025-09-16 21:27:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
-const mobileMenu = document.querySelector(".mobile-menu");
 const mobileSlide = document.getElementById("mobileSlide");
 const closeBtn = document.querySelector(".close-btn");
 
-// فتح/غلق القائمة العادية
-hamburger.addEventListener("click", () => {
-  mobileMenu.style.display =
-    mobileMenu.style.display === "block" ? "none" : "block";
-});
-
 // فتح القائمة slide
 hamburger.addEventListener("click", () => {
-  mobileSlide.style.width = "250px"; // العرض عند الفتح
+  mobileSlide.style.width = "250px";
 });
 
 // غلق القائمة slide
